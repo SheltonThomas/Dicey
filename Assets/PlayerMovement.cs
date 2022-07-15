c6439ab943ab525d8fb1f41e91c21ca346773b48
Flavien Missier <c00434996@louisiana.edu>
2022-07-15 17:02:41 -0500

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,10 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 5f;
+    public float rotationSpeed = 200f;
     public float hitboxSize = 1f;
     private LayerMask blockMask = 0;    // Start is called before the first frame update
     private Vector3 destination;
-
+    private Quaternion rotation;
     void Start()
     {
         
@@ -17,32 +18,36 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-
-        if(Vector3.Distance(transform.position, destination) < Mathf.Epsilon){
+        if (Vector3.Distance(transform.position, destination) < Mathf.Epsilon && !(Quaternion.Dot(transform.rotation, rotation) > 1f - Mathf.Epsilon))
+        {
             if(Input.GetAxis("Horizontal") > 0){
                 if(CheckDirection(Vector3.right)){
                     destination = transform.position + Vector3.right;
+                    rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, -90f, 0f));
                 }
             }
             if(Input.GetAxis("Horizontal") < 0){
                 if(CheckDirection(Vector3.left)){
                     destination = transform.position + Vector3.left;
+                    rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, 90f, 0f));
                 }
             }
             if(Input.GetAxis("Vertical") > 0){
                 if(CheckDirection(Vector3.up)){
                     destination = transform.position + Vector3.up;
+                    rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-90f, 0f, 0f));
                 }
             }
             if(Input.GetAxis("Vertical") < 0){
                 if(CheckDirection(Vector3.down)){
                     destination = transform.position + Vector3.down;
+                    rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(90f, 0f, 0f));
                 }
             }
-
         }
         else{
             transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
         }
     }
 
