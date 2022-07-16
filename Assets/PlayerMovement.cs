20fd55305f75240f57938d0d6c2cb1452ae1601f
Flavien Missier <c00434996@louisiana.edu>
2022-07-16 10:50:29 -0500

    Working movement and rotation
    Collision detection not working

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,67 +4,69 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Quaternion startingRotation;
+    public float speed = 25;
+    private bool moving = false;
 
-    public float speed = 25f;
-    public float rotationSpeed = 25f;
-    public float hitboxSize = 1f;
-    private LayerMask blockMask = 0;    // Start is called before the first frame update
+    public float rotationSpeed = 25;
+    public float hitboxSize = 1.5f;
+    private LayerMask blockMask = 0;
     private Vector3 destination;
-    private Quaternion rotation;
-    private bool moving;
-    void Start()
-    {
-        
+
+
+    void Start(){
     }
 
-    // Update is called once per frame
     void Update(){
         if (!moving)
         {
             if(Input.GetAxis("Horizontal") > 0){
                 if(CheckDirection(Vector3.right)){
+                    moving = true;
                     destination = transform.position + Vector3.right;
-                    rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, -90f, 0f));
-                    moving = true;
-                    StartCoroutine(RotateAndMove());
+                    StopAllCoroutines();
+                    StartCoroutine(Rotate(new Vector3(0, -90, 0)));
                 }
             }
-            if(Input.GetAxis("Horizontal") < 0){
+            else if(Input.GetAxis("Horizontal") < 0){
                 if(CheckDirection(Vector3.left)){
+                    moving = true;
                     destination = transform.position + Vector3.left;
-                    rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, 90f, 0f));
-                    moving = true;
-                    StartCoroutine(RotateAndMove());
+                    StopAllCoroutines();
+                    StartCoroutine(Rotate(new Vector3(0, 90, 0)));
                 }
             }
-            if(Input.GetAxis("Vertical") > 0){
+            else if(Input.GetAxis("Vertical") > 0){
                 if(CheckDirection(Vector3.up)){
-                    destination = transform.position + Vector3.up;
-                    rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-90f, 0f, 0f));
                     moving = true;
-                    StartCoroutine(RotateAndMove());
+                    destination = transform.position + Vector3.up;
+                    StopAllCoroutines();
+                    StartCoroutine(Rotate(new Vector3(90, 0, 0)));
                 }
             }
-            if(Input.GetAxis("Vertical") < 0){
+            else if(Input.GetAxis("Vertical") < 0){
                 if(CheckDirection(Vector3.down)){
-                    destination = transform.position + Vector3.down;
-                    rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(90f, 0f, 0f));
                     moving = true;
-                    StartCoroutine(RotateAndMove());
+                    destination = transform.position + Vector3.down;
+                    StopAllCoroutines();
+                    StartCoroutine(Rotate(new Vector3(-90, 0, 0)));
                 }
             }
         }
     }
 
-    IEnumerator RotateAndMove(){
-        Quaternion finalRotation = rotation * transform.rotation;
+    IEnumerator Rotate(Vector3 rotationAmount){
+        startingRotation = this.transform.rotation;
+        Quaternion finalRotation = Quaternion.Euler( rotationAmount.x, rotationAmount.y, rotationAmount.z ) * startingRotation;
         Vector3 finalPosition = destination;
-        while(this.transform.rotation != finalRotation && this.transform.position != finalPosition){
-            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, finalRotation, Time.deltaTime*rotationSpeed);
-            this.transform.position = Vector3.Lerp(this.transform.position, finalPosition, Time.deltaTime*speed);
+
+        while(this.transform.rotation != finalRotation){
+            float time = Time.deltaTime;
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, finalRotation, time * rotationSpeed);
+            this.transform.position = Vector3.Lerp(this.transform.position, finalPosition, time * speed);
             yield return 0;
         }
-            moving = false;
+        moving = false;
     }
 
     private bool CheckDirection(Vector3 direction){
