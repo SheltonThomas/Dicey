737232c7f2c975f52216cd6f773e74dd8b415891
Flavien Missier <c00434996@louisiana.edu>
2022-07-15 19:10:26 -0500

    Rotations are still sometimes wrong

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 5f;
-    public float rotationSpeed = 200f;
+    public float speed = 25f;
+    public float rotationSpeed = 25f;
     public float hitboxSize = 1f;
     private LayerMask blockMask = 0;    // Start is called before the first frame update
     private Vector3 destination;
     private Quaternion rotation;
+    private bool moving;
     void Start()
     {
         
@@ -18,37 +19,52 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        if (Vector3.Distance(transform.position, destination) < Mathf.Epsilon && !(Quaternion.Dot(transform.rotation, rotation) > 1f - Mathf.Epsilon))
+        if (!moving)
         {
             if(Input.GetAxis("Horizontal") > 0){
                 if(CheckDirection(Vector3.right)){
                     destination = transform.position + Vector3.right;
                     rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, -90f, 0f));
+                    moving = true;
+                    StartCoroutine(RotateAndMove());
                 }
             }
             if(Input.GetAxis("Horizontal") < 0){
                 if(CheckDirection(Vector3.left)){
                     destination = transform.position + Vector3.left;
                     rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, 90f, 0f));
+                    moving = true;
+                    StartCoroutine(RotateAndMove());
                 }
             }
             if(Input.GetAxis("Vertical") > 0){
                 if(CheckDirection(Vector3.up)){
                     destination = transform.position + Vector3.up;
                     rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-90f, 0f, 0f));
+                    moving = true;
+                    StartCoroutine(RotateAndMove());
                 }
             }
             if(Input.GetAxis("Vertical") < 0){
                 if(CheckDirection(Vector3.down)){
                     destination = transform.position + Vector3.down;
                     rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(90f, 0f, 0f));
+                    moving = true;
+                    StartCoroutine(RotateAndMove());
                 }
             }
         }
-        else{
-            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
+
+    IEnumerator RotateAndMove(){
+        Quaternion finalRotation = rotation * transform.rotation;
+        Vector3 finalPosition = destination;
+        while(this.transform.rotation != finalRotation && this.transform.position != finalPosition){
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, finalRotation, Time.deltaTime*rotationSpeed);
+            this.transform.position = Vector3.Lerp(this.transform.position, finalPosition, Time.deltaTime*speed);
+            yield return 0;
         }
+            moving = false;
     }
 
     private bool CheckDirection(Vector3 direction){
